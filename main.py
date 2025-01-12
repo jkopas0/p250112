3eb375928846928fb19a5167a9c8c707eb2394d4
Jakub Kopas <severebraindamage590@gmail.com>
2025-01-12 17:18:58 +0100

    Added support for parentheses

--- main.py
+++ main.py
@@ -3,22 +3,13 @@ class app:
 	def __init__(self):
 		self.user_input = ""
 		self.num_chars = "0123456789."
-		self.op_chars = "*/-+="
+		self.op_chars = "*/-+=()"
 		
 	def main(self):
 		while True:
 			self.user_input = input("> ") + '='
 			self.split_input_string()
-			if not self.perform_operation('*', lambda a, b: a * b):
-				print("Could not calculate.")
-				continue
-			if not self.perform_operation('/', lambda a, b: a / b):
-				print("Could not calculate.")
-				continue
-			if not self.perform_operation('+', lambda a, b: a + b):
-				print("Could not calculate.")
-				continue
-			if not self.perform_operation('-', lambda a, b: a - b):
+			if self.handle_parentheses(0) == -1:
 				print("Could not calculate.")
 				continue
 			print(f"= {self.user_input[0]}")
@@ -47,19 +38,46 @@ class app:
 			split_string.pop(-1)
 		self.user_input = split_string
 		
-	def perform_operation(self, op, func):
+	def perform_operation(self, op, pos, pos_end, func):
 		try:
-			for i in range(len(self.user_input)):
-				if self.user_input[i] == op:
-					x = func(self.user_input[i - 1], self.user_input[i + 1])
-					self.user_input[i - 1] = x
-					self.user_input[i] = ""
-					self.user_input[i + 1] = ""
-					i -= 1
-			self.user_input = [i for i in self.user_input if i != ""]
+			while pos < pos_end:
+				if self.user_input[pos] == op:
+					x = func(self.user_input[pos - 1], self.user_input[pos + 1])
+					self.user_input[pos - 1] = x
+					self.user_input[pos] = ""
+					self.user_input[pos + 1] = ""
+				pos += 1
 			return True
 		except TypeError:
 			return False
+			
+	def handle_parentheses(self, pos):
+		pos_start = pos
+		end = len(self.user_input)
+		
+		tmp = []
+		
+		while pos < end:
+			if self.user_input[pos] == '(':
+				self.user_input.pop(pos)
+				pos = self.handle_parentheses(pos)
+				if pos == -1: return -1
+			elif self.user_input[pos] == ')':
+				self.user_input.pop(pos)
+				if not self.perform_operation('*', pos_start, pos - 1, lambda a, b: a * b): return -1
+				if not self.perform_operation('/', pos_start, pos - 1, lambda a, b: a / b): return -1
+				if not self.perform_operation('+', pos_start, pos - 1, lambda a, b: a + b): return -1
+				if not self.perform_operation('-', pos_start, pos - 1, lambda a, b: a - b): return -1
+				self.user_input = [i for i in self.user_input if i != ""]
+				return pos_start
+			end = len(self.user_input)
+			pos += 1
+		if not self.perform_operation('*', pos_start, pos - 1, lambda a, b: a * b): return -1
+		if not self.perform_operation('/', pos_start, pos - 1, lambda a, b: a / b): return -1
+		if not self.perform_operation('+', pos_start, pos - 1, lambda a, b: a + b): return -1
+		if not self.perform_operation('-', pos_start, pos - 1, lambda a, b: a - b): return -1
+		self.user_input = [i for i in self.user_input if i != ""]
+		return pos_start
 		
 if __name__ == "__main__":
 	app = app()
