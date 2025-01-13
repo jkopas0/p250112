2a366470cbc110cb1e1b37cf784a52ea58796a9f
Jakub Kopas <severebraindamage590@gmail.com>
2025-01-13 23:25:26 +0100

    Rewrote main.py due to the amount of bugs

--- main.py
+++ main.py
@@ -1,97 +1,143 @@
 
-class app:
-	def __init__(self):
-		self.user_input = ""
-		self.num_chars = "0123456789."
-		self.op_chars = "*/-+^=()"
-		self.previous_calculation = 0.0
+num_chars = "1234567890."
+op_chars = "/*-+"
+
+def main():
+	while True:
+		user_input = input("> ")
+		array = prepare_input(user_input)
+		answer = calculate_loop(array)
+		print(f"= {answer}")
 		
-	def main(self):
-		while True:
-			self.user_input = input("> ") + '='
-			if not self.split_input_string():
-				print("No input has been provided.")
-				continue
-			if self.handle_parentheses(0) == -1:
-				print("Could not calculate.")
-				continue
-			try:
-				self.previous_calculation = self.user_input[0]
-				print(f"= {self.user_input[0]}")
-			except IndexError:
-				print("Could not calculate.")
-			
-	def split_input_string(self):
-		num_chars = self.num_chars
-		op_chars = self.op_chars
-		
-		split_string = []
-		
-		tmp = ""
-		
-		for char in self.user_input:
-			tmp_tmplen = len(tmp)
-			for num in num_chars:
-				if char == num:
-					tmp += char
-			if tmp_tmplen == len(tmp) and tmp != "":
-				tmp = float(tmp)
-				split_string.append(tmp)
+def prepare_input(string):
+	accepted_chars = num_chars + op_chars + "()"
+	
+	split_string = []
+	
+	for char in string:
+		for _char in accepted_chars:
+			if char == _char:
+				split_string.append(char)
+				
+	tmp = ""
+	tmp_array = []
+	
+	for char in split_string:
+		if char.isdigit() or char == '.':
+			tmp += char
+		elif char == '-':
+			if tmp:
+				tmp_array.append(tmp)
 				tmp = ""
-			for op in op_chars:
-				if char == op:
-					split_string.append(char)
-		try:
-			while split_string[-1] == '=':
-				split_string.pop(-1)
-		except IndexError:
-			return False
-		self.user_input = split_string
-		return True
-		
-	def handle_parentheses(self, pos):
-		pos_start = pos
-		end = len(self.user_input)
-		
-		tmp = []
-		
-		while pos < end:
-			if self.user_input[pos] == '(':
-				self.user_input.pop(pos)
-				pos = self.handle_parentheses(pos)
-				if pos == -1: return -1
-			elif self.user_input[pos] == ')':
-				self.user_input.pop(pos)
-				return self.handle_operations(pos_start, pos)
-			end = len(self.user_input)
-			pos += 1
-		return self.handle_operations(pos_start, pos)
-		
-	def handle_operations(self, pos_start, pos):
-		if not self.perform_operation('^', pos_start, pos - 1, lambda a, b: a ** b): return -1
-		if not self.perform_operation('*', pos_start, pos - 1, lambda a, b: a * b): return -1
-		if not self.perform_operation('/', pos_start, pos - 1, lambda a, b: a / b): return -1
-		if not self.perform_operation('+', pos_start, pos - 1, lambda a, b: a + b): return -1
-		if not self.perform_operation('-', pos_start, pos - 1, lambda a, b: a - b): return -1
-		self.user_input = [i for i in self.user_input if i != ""]
-		return pos_start
-		
-	def perform_operation(self, op, pos, pos_end, func):
-		try:
-			while pos < pos_end:
-				if self.user_input[pos] == op:
-					x = func(self.user_input[pos - 1], self.user_input[pos + 1])
-					self.user_input[pos - 1] = x
-					self.user_input[pos] = ""
-					self.user_input[pos + 1] = ""
-				pos += 1
-			return True
-		except TypeError:
-			return False
+			tmp_array.append('+')
+			tmp += char
+		elif char == '(':
+			if tmp:
+				tmp_array.append(tmp)
+				tmp = ""
+			tmp_array.append('*')
+			tmp_array.append(char)
+		elif char == ')':
+			if tmp:
+				tmp_array.append(tmp)
+				tmp = ""
+			tmp_array.append(char)
+		else:
+			if tmp:
+				tmp_array.append(tmp)
+				tmp = ""
+			tmp_array.append(char)
 			
+	if tmp:
+		tmp_array.append(tmp)
+	
+	split_string = tmp_array
+	
+	for i in range(len(split_string)):
+		try:
+			split_string[i] = float(split_string[i])
+		except ValueError:
+			pass
+			
+	split_string = [i for i in split_string if not (isinstance(i, str) and '.' in i)]
+	
+	tmp = []
+	tmp_array = []
+	
+	for i in split_string:
+		if i == '(':
+			tmp.append(tmp_array)
+			tmp_array = []
+		
+		elif i == ')':
+			if tmp:
+				last = tmp.pop()
+				last.append(tmp_array)
+				tmp_array = last
+		
+		else:
+			tmp_array.append(i)
+			
+	split_string = tmp_array if not tmp else []
+	
+	split_string = clean_array_loop(split_string)
+	
+	return split_string
+	
+def clean_array_loop(array):
+	for i in range(len(array)):
+		if isinstance(array[i], list):
+			array[i] = clean_array_loop(array[i])
+	return clean_array(array)
+	
+def clean_array(array):
+	tmp_array = []
+	
+	for i in array:
+		if isinstance(i, float):
+			if not tmp_array or isinstance(tmp_array[-1], str):
+				tmp_array.append(i)
+		
+		elif isinstance(i, str) and i in op_chars:
+			if tmp_array and isinstance(tmp_array[-1], (float, list)):
+				tmp_array.append(i)
+				
+		elif isinstance(i, list):
+			tmp_array.append(i)
+				
+	if isinstance(tmp_array[-1], str):
+		tmp_array.pop(-1)
+		
+	return tmp_array
+
+def calculate_loop(array):
+	for i in range(len(array)):
+		if isinstance(array[i], list):
+			array[i] = calculate_loop(array[i])
+	array = calculate(array, ('*', '/'), (lambda a, b: a * b, lambda a, b: a / b))
+	array = calculate(array, ('+', '-'), (lambda a, b: a + b, lambda a, b: a - b))
+	return array[0]
+
+def calculate(array, ops, funcs):
+	tmp_array = []
+	
+	for i in range(len(array)):
+		if isinstance(array[i], str) and array[i] in ops:
+			for j in range(len(ops)):
+				if array[i] == ops[j]:
+					array[i + 1] = funcs[j](array[i - 1], array[i + 1])
+			
+		elif isinstance(array[i], str) and array[i] not in ops:
+			tmp_array.append(array[i - 1])
+			tmp_array.append(array[i])
+			
+		elif i == len(array) - 1:
+			tmp_array.append(array[i])
+			
+	return tmp_array
+
 if __name__ == "__main__":
-	app = app()
 	try:
-		app.main()
+		main()
 	except KeyboardInterrupt:
 		print("\nClosing...")
